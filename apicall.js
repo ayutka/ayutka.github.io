b6b4242280869fbd2ea0cf60244b1aebb310949b
ayutka <juventus1015@outlook.jp>
2021-11-17 16:39:13 +0900

    1,2のボタン機能を追加

--- apicall.js
+++ apicall.js
@@ -1,6 +1,6 @@
 /** @format */
 
-//
+//最初の計算式
 const submit = document.getElementById("submit");
 submit.addEventListener("click", function () {
   //clickがくるまでaddが待って、来たらfunctionに中身が入る
@@ -9,5 +9,17 @@ submit.addEventListener("click", function () {
   console.log(encoded_formula);
   fetch(`http://0.0.0.0:8080/?formula=${encoded_formula}`) //url先にある文字列を受け取ってくる関数
     .then((response) => response.json()) //受け取った文字列をjsonに変換
-    .then((data) => document.getElementById("ans").innerHTML =`${data.ans}`); //上の内容がdataに入っている dataの辞書(型配列)ansにある要素を出力
+    .then((data) => (document.getElementById("ans").innerHTML = `${data.ans}`)); //上の内容がdataに入っている dataの辞書(型配列)ansにある要素を出力
 });
+
+//ボタン1を追加
+const buttonUL = document.querySelector(".buttonUL");
+const numberButton = buttonUL.querySelectorAll("button");
+for (let i = 0; i < numberButton.length; i++) {
+  numberButton[i].addEventListener("click", function (event) {
+    const formula = document.getElementById("formula").value;
+    const number = event.target.value;
+    const newFormula = `${formula}${number}`; //formulaの後ろに1を追加
+    document.getElementById("formula").value = newFormula;
+  });
+}
