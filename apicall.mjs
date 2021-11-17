de844d8cd4cf306620543fa8a4f594fcd0ec0026
ayutka <juventus1015@outlook.jp>
2021-11-17 15:56:04 +0900

    不要なファイルを削除

--- apicall.mjs
+++
@@ -1,9 +0,0 @@
-import fetch from "node-fetch";
-
-const formula = "2 + 3"
-
-const encoded_formula = encodeURIComponent(formula);
-console.log(encoded_formula)
-fetch(`http://0.0.0.0:8080/?formula=${encoded_formula}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
