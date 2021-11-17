8ad67077897a57738b3667c1b29007bc7c111744
ayutka <juventus1015@outlook.jp>
2021-11-17 20:50:17 +0900

    不要なconsole.logを削除

--- apicall.js
+++ apicall.js
@@ -6,7 +6,6 @@ submit.addEventListener("click", function () {
   //clickがくるまでaddが待って、来たらfunctionに中身が入る
   const formula = document.getElementById("formula").value; //formulaていうIDの中にある要素のformulaをゲットした
   const encoded_formula = encodeURIComponent(formula); //'+'や'='など特殊文字もencodeする
-  console.log(encoded_formula);
   fetch(`http://0.0.0.0:8080/?formula=${encoded_formula}`) //url先にある文字列を受け取ってくる関数
     .then((response) => response.json()) //受け取った文字列をjsonに変換
     .then((data) => (document.getElementById("ans").innerHTML = `${data.ans}`)); //上の内容がdataに入っている dataの辞書(型配列)ansにある要素を出力
